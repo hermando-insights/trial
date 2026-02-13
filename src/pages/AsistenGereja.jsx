7df39c2fa397935cba8eeb23e35e451bc1f9c3bb
hermando-insight <dvdhermando@gmail.com>
2026-02-13 11:29:47 +0700

    update api link ke render untuk asisten gereja

--- src/pages/AsistenGereja.jsx
+++ src/pages/AsistenGereja.jsx
@@ -28,7 +28,7 @@ const AsistenGereja = () => {
   // Fungsi kirim data ke Backend Flask
   const generatePPT = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/generate-ppt', {
+      const response = await fetch('https://asisten-gereja-anda.onrender.com/generate-ppt', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ slides: slides }),
